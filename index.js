3f4bd66b368ea7445121d5b2af917935a12fb4ef
brady <bradybill36@gmail.com>
2023-07-06 16:50:49 -0500

    saw that we had to include classes for the shapes, so i had to completely redo my shapes, now im gonna do my readme/video and submit it

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const fs = require('fs');
 const inquirer = require('inquirer');
-const shapes = require('./lib/shapes');
+const { Circle, Triangle, Square} = require('./lib/shapes');
 const questions = require('./lib/questions');
 
 const prompt = inquirer.createPromptModule();
 
 prompt(questions)
     .then((answers) => {
-        const svgContent = shapes[answers.shape](answers);
+        const svgContent = answers.shape === 'Circle' ? new Circle(answers.shapeColor, answers.textColor, answers.text).render() :
+            answers.shape === 'Triangle' ? new Triangle(answers.shapeColor, answers.textColor, answers.text).render() :
+            answers.shape === 'Square' ? new Square(answers.shapeColor, answers.textColor, answers.text).render() : '';
 
         fs.writeFile(answers.name, svgContent, (err) => {
             if (err) throw err;
